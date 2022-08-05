8a9d81195d51b371c18e72830830282e8057a199
nusaibatron <nusaiba@berkeley.edu>
2022-08-05 18:47:00 -0400

    Add Day 5

---
+++ Day4.swift
@@ -0,0 +1,14 @@
+/*Type Annotations*/
+
+// type annotation, then asssign empty data structure
+var sport: String = ""
+// type inference
+var colorArray: [String] = []
+var colorArray2 = [String]()
+
+// useful way to use
+enum UIStyle {case light, dark, sytem}
+var style = UIStyle.light
+
+//below will error
+//let score: Int = "zero"
